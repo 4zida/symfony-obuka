e8bd1a61ca6d6142cdea53ec70accffbc257ee9d
Veljko Bogdan <vtsveljkobogdan@gmail.com>
2024-06-28 11:30:39 +0200

    User and Company tests started

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -2,48 +2,69 @@
 
 namespace App\Tests\Controller;
 
-use App\Controller\UserController;
-use PHPUnit\Framework\TestCase;
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class UserControllerTest extends TestCase
+class UserControllerTest extends KernelTestCase
 {
+    private ?EntityManager $entityManager;
 
-    public function testIndex()
+    protected function setUp(): void
+    {
+        $kernel = static::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    public function testIndex(): void
+    {
+        $users = $this->entityManager
+            ->getRepository(User::class)
+            ->findAll();
+
+        $this->assertNotEmpty($users);
+        $this->assertIsArray($users);
+
+        foreach ($users as $user)
+        {
+            $this->assertInstanceOf(User::class, $user);
+        }
+    }
+
+    public function testUpdate(): void
     {
         $this->markTestIncomplete();
     }
 
-    public function testUpdate()
+    public function testFindByCompany(): void
     {
         $this->markTestIncomplete();
     }
 
-    public function testFindByCompany()
+    public function testDelete(): void
     {
         $this->markTestIncomplete();
     }
 
-    public function testDelete()
+    public function testCreate(): void
     {
         $this->markTestIncomplete();
     }
 
-    public function testCreate()
+    public function testFindById(): void
     {
         $this->markTestIncomplete();
     }
 
-    public function testFindById()
+    public function testFindByRole(): void
     {
         $this->markTestIncomplete();
     }
 
-    public function testFindByRole()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function testShow()
+    public function testShow(): void
     {
         $this->markTestIncomplete();
     }
