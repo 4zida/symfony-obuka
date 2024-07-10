e3bdbd14846c9481c61a99e974bca2f818158f74
Veljko Bogdan <vtsveljkobogdan@gmail.com>
2024-07-10 11:54:05 +0200

    AdController testShow and setUp

--- tests/Controller/AdControllerTest.php
+++ tests/Controller/AdControllerTest.php
@@ -2,12 +2,35 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
 use Nebkam\FluentTest\RequestBuilder;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
 
 class AdControllerTest extends WebTestCase
 {
+    private static User $agent;
+    private static ?int $agentId;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        self::$agent = (new User())
+            ->setEmail('agent@example.com')
+            ->setRoles(['ROLE_AGENT'])
+            ->setPasswordNoHash('12345678')
+            ->setPassword('12345678')
+            ->setRole('TestRole')
+            ->setName('TestName')
+            ->setSurname('TestSurname')
+            ->setCompany(null)
+        ;
+        self::$agentId = self::$agent->getId();
+
+        self::ensureKernelShutdown();
+    }
+
     public function testIndex() : void
     {
         $response = RequestBuilder::create(self::createClient())
@@ -18,6 +41,20 @@ class AdControllerTest extends WebTestCase
 
         $content = $response->getJsonContent();
         dump($content);
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $response = RequestBuilder::create(self::createClient())
+            ->setMethod(Request::METHOD_GET)
+            ->setUri('/api/ad/%d', self::$agentId)
+            ->getResponse();
+        self::assertResponseIsSuccessful();
+
+        $content = $response->getJsonContent();
+        self::assertNotEmpty($content);
+        self::assertEquals(self::$agentId, $content['id']);
     }
 
     public function testCreate() : void
