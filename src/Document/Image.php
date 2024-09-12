4cf4a0b90c582250bda248e2599b065921fc9927
Veljko Bogdan <vtsveljkobogdan@gmail.com>
2024-09-12 11:53:46 +0200

    Image and repository created

---
+++ src/Document/Image.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Document;
+
+use App\Repository\ImageRepository;
+use App\Util\ContextGroup;
+use DateTimeImmutable;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[MongoDB\Document(collection: 'image', repositoryClass: ImageRepository::class)]
+#[Groups(ContextGroup::IMAGE_DETAILS)]
+class Image
+{
+    #[MongoDB\Field(type: 'string')]
+    #[MongoDB\Id]
+    protected ?string $id;
+    #[MongoDB\Field(type: 'string')]
+    protected ?string $alias;
+    #[MongoDB\Field(type: 'string')]
+    #[Assert\NotBlank]
+    protected ?string $location;
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?string $location): self
+    {
+        $this->location = $location;
+        return $this;
+    }
+    #[MongoDB\Field(type: 'date_immutable')]
+    protected ?DateTimeImmutable $createdAt;
+    #[MongoDB\ReferenceOne(targetDocument: Ad::class, orphanRemoval: true)]
+    protected ?Ad $ad;
+
+    #[Groups([
+
+    ])]
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    #[Groups([
+
+    ])]
+    public function getAlias(): string
+    {
+        return $this->alias;
+    }
+
+    public function setAlias(string $alias): self
+    {
+        $this->alias = $alias;
+        return $this;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    #[Groups([
+
+    ])]
+    public function getCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->createdAt ?? null;
+    }
+
+    public function setAd(Ad $ad): self
+    {
+        $this->ad = $ad;
+        return $this;
+    }
+}
