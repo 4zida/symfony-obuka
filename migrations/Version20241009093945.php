86e184a4ccd82c1a99a40cb0e37073e4322c3a0a
Veljko Bogdan <vtsveljkobogdan@gmail.com>
2024-10-09 11:56:18 +0200

    update

---
+++ migrations/Version20241009093945.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241009093945 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, about_us LONGTEXT NOT NULL, created_at DATE NOT NULL, is_active TINYINT(1) NOT NULL, latitude NUMERIC(5, 2) NOT NULL, longitude NUMERIC(5, 2) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE phone (id INT AUTO_INCREMENT NOT NULL, full VARCHAR(64) NOT NULL, national VARCHAR(64) DEFAULT NULL, international VARCHAR(64) DEFAULT NULL, isViber TINYINT(1) DEFAULT NULL, countryCode VARCHAR(64) NOT NULL, user_id INT DEFAULT NULL, INDEX IDX_444F97DDA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, password_no_hash VARCHAR(255) NOT NULL, roles LONGTEXT DEFAULT NULL, created_at DATE NOT NULL, is_active TINYINT(1) NOT NULL, last_seen_at DATE NOT NULL, company_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), INDEX IDX_8D93D649979B1AD6 (company_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE phone ADD CONSTRAINT FK_444F97DDA76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE `user` ADD CONSTRAINT FK_8D93D649979B1AD6 FOREIGN KEY (company_id) REFERENCES company (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE phone DROP FOREIGN KEY FK_444F97DDA76ED395');
+        $this->addSql('ALTER TABLE `user` DROP FOREIGN KEY FK_8D93D649979B1AD6');
+        $this->addSql('DROP TABLE company');
+        $this->addSql('DROP TABLE phone');
+        $this->addSql('DROP TABLE `user`');
+    }
+}
