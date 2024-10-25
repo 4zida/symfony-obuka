06a71ef060dce2a6052ee2bb5c2b501b4d555d10
Veljko Bogdan <vtsveljkobogdan@gmail.com>
2024-10-25 12:19:04 +0200

    Codes for custom exceptions

--- src/Exception/ClosedCreditBalanceException.php
+++ src/Exception/ClosedCreditBalanceException.php
@@ -3,11 +3,12 @@
 namespace App\Exception;
 
 use Exception;
+use Symfony\Component\HttpFoundation\Response;
 
 class ClosedCreditBalanceException extends Exception
 {
     public function __construct()
     {
-        parent::__construct('You have no permission to spend credits!');
+        parent::__construct('You have no permission to spend credits!', Response::HTTP_FORBIDDEN);
     }
 }
--- src/Exception/InsufficientCreditsException.php
+++ src/Exception/InsufficientCreditsException.php
@@ -3,11 +3,12 @@
 namespace App\Exception;
 
 use Exception;
+use Symfony\Component\HttpFoundation\Response;
 
 class InsufficientCreditsException extends Exception
 {
     public function __construct()
     {
-        parent::__construct('Not enough credits!');
+        parent::__construct('Not enough credits!', Response::HTTP_PAYMENT_REQUIRED);
     }
 }
